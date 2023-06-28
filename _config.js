c960f7fe61d83c3159630a788ff1a907d1bf7f00
Okioimike <okioibonyi@gmail.com>
2023-06-29 02:54:57 +0300

    Added Jenkins file

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+// Update to have your correct okioi and WmIkAl79079
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    
+    production: 'mongodb+srv://okioi:WmIkAl79079@gallery.wc344.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://okioi:WmIkAl79079@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://okioi:WmIkAl79079@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
